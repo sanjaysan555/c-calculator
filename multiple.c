a078cb28c987ca5755d7d65fc64207f7c818ea4c
sanjaysan555 <sanjaysan7106@gamil.com>
2025-02-08 15:30:28 +0530

    for multiple

---
+++ multiple.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int x,y;
+int c;
+void multiple(int x,int y)
+{
+    printf("enter the first num:\n");
+    scanf("%d",&a);
+    printf("enter the seconf num:\n");
+    scanf("%d",&y);
+    c =a*b;
+    printf("answer:%d",c);
+}
+}
